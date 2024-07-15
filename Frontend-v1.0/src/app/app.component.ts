bc7964c0354f97ac635b8254b94b102b067f5bf6
Theodoros Moraitis <theodorosmoraitis2002@gmail.com>
2024-07-15 17:19:52 +0300

    fast update

--- Frontend-v1.0/src/app/app.component.ts
+++ Frontend-v1.0/src/app/app.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'Frontend-v1.0';
+  email: string = '';
+  password: string = '';
+  msg: string = '';
+
+  constructor(private router: Router) {}
+
+  onLogIn() {
+    if (this.email === '' || this.password === '') {
+      this.msg = 'Please enter both fields';
+
+      setTimeout(() => this.msg = '', 3000);  //set a timer for the time the error will show
+    } else {
+      console.log('Email:', this.email);
+      console.log('Password:', this.password);
+      //logic here
+
+      //Clear the form fields
+      this.email = '';
+      this.password = '';
+    }
+  }
+
+  onSignUp() {
+    //Redirect to the sign-up page
+    console.log('Redirect to sign-up page');
+    this.router.navigate(['/...']); //here we need to change the directory component route as needed
+  }
 }
