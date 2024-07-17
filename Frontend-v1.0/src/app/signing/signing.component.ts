496a5141eb1aee517a4d712163b9ebd0c63fc870
NikitasMosch <sdi2000135@di.uoa.gr>
2024-07-17 13:19:16 +0300

    signing-comp

---
+++ Frontend-v1.0/src/app/signing/signing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signing',
+  standalone: true,
+  imports: [],
+  templateUrl: './signing.component.html',
+  styleUrl: './signing.component.css'
+})
+export class SigningComponent {
+
+}
