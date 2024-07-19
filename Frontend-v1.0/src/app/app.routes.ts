1fb7d4ed79b4f8d0ec4ebb40d98dd6d7f11792a6
Theodoros Moraitis <theodorosmoraitis2002@gmail.com>
2024-07-19 13:06:53 +0300

    fast update (make git)

--- Frontend-v1.0/src/app/app.routes.ts
+++ Frontend-v1.0/src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { UserPageComponent } from './user-page/user-page.component';
 import { SigningComponent } from './signing/signing.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SettingsComponent } from './settings/settings.component';
 
 export const routes: Routes = [
   { path: 'login-page', component: LoginPageComponent },
   { path: 'user-page', component: UserPageComponent },
   { path: 'signing', component: SigningComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'settings', component: SettingsComponent },
 
   { path: '', redirectTo: '/login-page', pathMatch: 'full' }, //default route
   { path: '**', redirectTo: '/login-page' } //handle undefined routes
