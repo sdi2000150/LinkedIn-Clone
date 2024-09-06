ddf0265b1ad0834dc2cb7321969974624b0c1415
Theodoros Moraitis <theodorosmoraitis2002@gmail.com>
2024-09-06 14:02:48 +0300

    fast update (make git)

--- Frontend-v1.0/src/app/app.routes.ts
+++ Frontend-v1.0/src/app/app.routes.ts
@@ -6,18 +6,20 @@ import { SignupPageComponent } from './components/signup-page/signup-page.compon
 import { ProfileComponent } from './components/profile/profile.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 
 export const routes: Routes = [
+  { path: 'welcome-page', component: WelcomePageComponent },
   { path: 'login-page', component: LoginPageComponent },
   { path: 'signup-page', component: SignupPageComponent },
 
-  { path: 'navbar', component: NavbarComponent },
   { path: 'user-page', component: UserPageComponent },
   { path: 'admin-page', component: AdminPageComponent },
+  { path: 'navbar', component: NavbarComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'settings', component: SettingsComponent },
 
 
-  { path: '', redirectTo: '/login-page', pathMatch: 'full' }, //default route
-  { path: '**', redirectTo: '/login-page' } //handle undefined routes
+  { path: '', redirectTo: '/welcome-page', pathMatch: 'full' }, //default route
+  { path: '**', redirectTo: '/welcome-page' } //handle undefined routes
 ];
---
+++ Frontend-v1.0/src/app/components/welcome-page/welcome-page.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { RouterOutlet, Router, RouterLink, RouterLinkActive} from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-welcome-page',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, FormsModule, RouterLink, RouterLinkActive],
+  templateUrl: './welcome-page.component.html',
+  styleUrl: './welcome-page.component.css'
+})
+export class WelcomePageComponent {
+
+  constructor(private router: Router) {} //for usage in this.router.navigate
+
+  onLogIn() {
+    //Redirect to the login page
+    console.log('Redirect to login page');
+    this.router.navigate(['../login-page']); //here we need to change the directory component route as needed
+  }
+
+  onSignUp() {
+    //Redirect to the sign-up page
+    console.log('Redirect to sign-up page');
+    this.router.navigate(['../signup-page']); //here we need to change the directory component route as needed
+  }
+}
